d75520e048816c76999a24f4398b8d222a1f6b8a
wagner <wagner.caixeta@gmail.com>
2020-02-03 21:25:02 -0300

    Create internal_cache_time to avoid query redis too much

--- lib/standby.rb
+++ lib/standby.rb
@@ -11,7 +11,10 @@ require 'standby/active_record/log_subscriber'
 
 module Standby
   class << self
-    attr_accessor :disabled, :check_rails_cache
+    attr_accessor :disabled, :check_rails_cache, :internal_cache_time
+    
+    @@last_value = nil
+    @@expires_on = nil
 
     def standby_connections
       @standby_connections ||= {}
@@ -25,12 +28,13 @@ module Standby
     def on_primary(&block)
       Base.new(:primary).run &block
     end
-
+    
     def global_disabled?
+      return @@last_value if @@expires_on.present? && @@expires_on > Time.now
       return false unless check_rails_cache
-      stored_value = Rails.cache.read('standby_enabled')
-      return false if stored_value == true
-      true
+      @@last_value = !(Rails.cache.read('standby_enabled') == true)
+      @@expires_on = Time.now + internal_cache_time if internal_cache_time.present?
+      @@last_value
     end
   end
 end
